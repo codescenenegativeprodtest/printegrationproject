3b8842e5f566bb61e66211ac694458b8f604c357
Mahir Sehalic <mahir.sehalic@systemverification.com>
2024-08-30 13:20:09 +0200

    update App.java

--- examples/guice-demo/src/main/java/com/example/App.java
+++ examples/guice-demo/src/main/java/com/example/App.java
@@ -20,7 +20,159 @@ public class App {
   @Inject
   App(Printer printer, @Message String message) {
     this.printer = printer;
+    this.message = message;  App(Printer printer, @Message String message) {
+    this.printer = printer;
     this.message = message;
+  }
+
+  public void run() {
+    printer.printMessage(message);
+  }
+
+  public static void main(String[] args) {
+    Printer consolePrinter =
+        new Printer() {
+          @Override
+          public void printMessage(String message) {
+            System.out.println(message);
+          }
+        };
+    App app =
+        Guice.createInjector(
+                new MessageModule(),
+                new AbstractModule() {
+                  @Override
+                  protected void configure() {
+                    bind(Printer.class).toInstance(consolePrinter);
+                  }
+                })
+            .getInstance(App.class);
+    app.run();
+  }
+
+  static class MessageModule extends AbstractModule {
+    @Provides
+    @Message
+    String provideMessage() {
+      return "Hello, Guice!";
+    }
+  }
+}
+  App(Printer printer, @Message String message) {
+    this.printer = printer;
+    this.message = message;
+  }
+
+  public void run() {
+    printer.printMessage(message);
+  }
+
+  public static void main(String[] args) {
+    Printer consolePrinter =
+        new Printer() {
+          @Override
+          public void printMessage(String message) {
+            System.out.println(message);
+          }
+        };
+    App app =
+        Guice.createInjector(
+                new MessageModule(),
+                new AbstractModule() {
+                  @Override
+                  protected void configure() {
+                    bind(Printer.class).toInstance(consolePrinter);
+                  }
+                })
+            .getInstance(App.class);
+    app.run();
+  }
+
+  static class MessageModule extends AbstractModule {
+    @Provides
+    @Message
+    String provideMessage() {
+      return "Hello, Guice!";
+    }
+  }
+}
+  App(Printer printer, @Message String message) {
+    this.printer = printer;
+    this.message = message;
+  }
+
+  public void run() {
+    printer.printMessage(message);
+  }
+
+  public static void main(String[] args) {
+    Printer consolePrinter =
+        new Printer() {
+          @Override
+          public void printMessage(String message) {
+            System.out.println(message);
+          }
+        };
+    App app =
+        Guice.createInjector(
+                new MessageModule(),
+                new AbstractModule() {
+                  @Override
+                  protected void configure() {
+                    bind(Printer.class).toInstance(consolePrinter);
+                  }
+                })
+            .getInstance(App.class);
+    app.run();
+  }
+
+  static class MessageModule extends AbstractModule {
+    @Provides
+    @Message
+    String provideMessage() {
+      return "Hello, Guice!";
+    }
+  }
+}
+  App(Printer printer, @Message String message) {
+    this.printer = printer;
+    this.message = message;
+  }
+
+  public void run() {
+    printer.printMessage(message);
+  }
+
+  public static void main(String[] args) {
+    Printer consolePrinter =
+        new Printer() {
+          @Override
+          public void printMessage(String message) {
+            System.out.println(message);
+          }
+        };
+    App app =
+        Guice.createInjector(
+                new MessageModule(),
+                new AbstractModule() {
+                  @Override
+                  protected void configure() {
+                    bind(Printer.class).toInstance(consolePrinter);
+                  }
+                })
+            .getInstance(App.class);
+    app.run();
+  }
+
+  static class MessageModule extends AbstractModule {
+    @Provides
+    @Message
+    String provideMessage() {
+      return "Hello, Guice!";
+    }
+  }
+}
+
   }
 
   public void run() {
